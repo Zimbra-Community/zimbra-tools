ffb20f7cd8556ddc9144c42efe9fce27b1d969bb
barrydegraaff <info@barrydegraaff.tk>
2016-06-08 07:47:10 +0200

    rewrote cbpolicyd.sh installer to use shipped cbpolicyd fix: https://github.com/Zimbra-Community/zimbra-tools/issues/1

--- cbpolicyd.sh
+++ cbpolicyd.sh
@@ -1,29 +1,40 @@
 #!/bin/bash
+
+# Copyright (C) 2016  Barry de Graaff
+# 
+# Bugs and feedback: https://github.com/Zimbra-Community/zimbra-tools/issues
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see http://www.gnu.org/licenses/.
+
 set -e
 # if you want to trace your script uncomment the following line
 # set -x
-
-
 # Documentation used from https://www.zimbrafr.org/forum/topic/7623-poc-zimbra-policyd/
 # https://wiki.zimbra.com/wiki/Postfix_Policyd#Example_Configuration
+# Thanks 
 
-# enhancement check package manager to choose between yum or aptitude
+echo "Automated cbpolicd installer for single-server Zimbra 8.6 on CentOS 6 or 7 (Ubuntu untested)
+- Installs policyd on MariaDB or MySQL (shipped with Zimbra) and show commands on how to activate on Zimbra
+- No webui is installed"
 
-echo "Automated cbpolicd installer for single-server Zimbra 8.6 on CentOS 6 or 7"
+# Make sure only root can run our script
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
 
-# CentOS 7
-yum -y groupinstall MariaDB\ Database\ Client
-# CentOS 6
-yum -y groupinstall MySQL\ Database\ client
-yum install -y epel-release 
-yum install -y pwgen
-
-rm policyd-master* -Rf
-
-wget "https://gitlab.devlabs.linuxassist.net/policyd/policyd/repository/archive.zip?ref=master" -O cbpolicyd-archive.zip
-unzip cbpolicyd-archive.zip
-
-CBPOLICYD_PWD=$(pwgen 10 -N1)
+CBPOLICYD_PWD=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-10};echo;)
 
 # creating a user, just to make sure we have one (for mysql on CentOS 6, so we can execute the next mysql queries w/o errors)
 cat <<EOF > /tmp/policyd-install.sql
@@ -33,46 +44,47 @@ GRANT ALL PRIVILEGES ON policyd_db . * TO 'ad-policyd_db'@'127.0.0.1' WITH GRANT
 FLUSH PRIVILEGES ; 
 EOF
 
-# need enhancement here with the use of zimbra env settings avoiding zmlocalconfig command
-mysql --force --host=127.0.0.1 --port=7306 --user=root --password=$(su zimbra -c "/opt/zimbra/bin/zmlocalconfig -s | grep mysql | grep ^mysql_root_password" | awk '{print $3}') < /tmp/policyd-install.sql > /dev/null 2>&1
+/opt/zimbra/bin/mysql --force < /tmp/policyd-install.sql > /dev/null 2>&1
 
-<<EOF > /tmp/policyd-install.sql
+cat <<EOF > /tmp/policyd-install.sql
 DROP USER 'ad-policyd_db'@'127.0.0.1';
-DROP DATABASE IF EXISTS policyd_db;
+DROP DATABASE policyd_db;
 CREATE DATABASE policyd_db CHARACTER SET 'UTF8'; 
 CREATE USER 'ad-policyd_db'@'127.0.0.1' IDENTIFIED BY '${CBPOLICYD_PWD}'; 
 GRANT ALL PRIVILEGES ON policyd_db . * TO 'ad-policyd_db'@'127.0.0.1' WITH GRANT OPTION; 
 FLUSH PRIVILEGES ; 
 EOF
 
-# need enhancement here with the use of zimbra env settings avoiding zmlocalconfig command
 cat /tmp/policyd-install.sql
-mysql --host=127.0.0.1 --port=7306 --user=root --password=$(su zimbra -c "/opt/zimbra/bin/zmlocalconfig -s | grep mysql | grep ^mysql_root_password" | awk '{print $3}') < /tmp/policyd-install.sql
+/opt/zimbra/bin/mysql < /tmp/policyd-install.sql
 
 echo "For your reference the database policyd_db and user have been created using: /tmp/policyd-install.sql"
 
-cd policyd-master*/database/
-
+#shipped version from Zimbra
+cd /opt/zimbra/cbpolicy*/share/database/
 for i in core.tsql access_control.tsql quotas.tsql amavis.tsql checkhelo.tsql checkspf.tsql greylisting.tsql accounting.tsql; 
 	do 
 	./convert-tsql mysql $i;
 	done > /tmp/policyd.sql
 
+# have to replace TYPE=InnoDB with ENGINE=InnoDB, this is not needed when using the latest upstream version of cbpolicyd
+# but it seems to be an issue in the version shipped with Zimbra
+grep -lZr -e "TYPE=InnoDB" "/tmp/policyd.sql" | xargs -0 sed -i "s^TYPE=InnoDB^ENGINE=InnoDB^g"
+
 echo "Backing up /opt/zimbra/conf/cbpolicyd.conf.in in /tmp/"
 cp -a /opt/zimbra/conf/cbpolicyd.conf.in /tmp/cbpolicyd.conf.in.$(date +%s)
 
 echo "Please wait... policyd_db populating..."
-# need enhancement here with the use of zimbra env settings avoiding zmlocalconfig command
-mysql --host=127.0.0.1 --port=7306 --user=root --password=$(su zimbra -c "/opt/zimbra/bin/zmlocalconfig -s | grep mysql | grep ^mysql_root_password" | awk '{print $3}') policyd_db < /tmp/policyd.sql
+/opt/zimbra/bin/mysql policyd_db < /tmp/policyd.sql
 echo "For your reference the database policyd_db populated using: /tmp/policyd.sql"
 
-echo "Setting username in /opt/zimbra/conf/cbpolicyd.conf.in:"
+echo "Setting username in /opt/zimbra/conf/cbpolicyd.conf.in"
 grep -lZr -e ".*sername=.*$" "/opt/zimbra/conf/cbpolicyd.conf.in" | xargs -0 sed -i "s^.*sername=.*$^Username=ad-policyd_db^g"
 
-echo "Setting password in /opt/zimbra/conf/cbpolicyd.conf.in:"
+echo "Setting password in /opt/zimbra/conf/cbpolicyd.conf.in"
 grep -lZr -e ".*assword=.*$" "/opt/zimbra/conf/cbpolicyd.conf.in"  | xargs -0 sed -i "s^.*assword=.*$^Password=${CBPOLICYD_PWD}^g"
 
-echo "Setting database in /opt/zimbra/conf/cbpolicyd.conf.in:"
+echo "Setting database in /opt/zimbra/conf/cbpolicyd.conf.in"
 grep -lZr -e "DSN=.*$" "/opt/zimbra/conf/cbpolicyd.conf.in"  | xargs -0 sed -i "s^DSN=.*$^DSN=DBI:mysql:database=policyd_db;host=127.0.0.1;port=7306^g"
 
 cat <<EOF > /tmp/policyd-policy.sql
@@ -89,16 +101,15 @@ echo "Setting basic policy
 - Rate limit any @domain from receiving more then 50 emails in a 60 second period. Messages beyond this rate are rejected.
 /tmp/policyd-policy.sql"
 
-mysql --host=127.0.0.1 --port=7306 --user=root --password=$(su zimbra -c "/opt/zimbra/bin/zmlocalconfig -s | grep mysql | grep ^mysql_root_password" | awk '{print $3}') policyd_db < /tmp/policyd-policy.sql
+/opt/zimbra/bin/mysql policyd_db < /tmp/policyd-policy.sql
 
 echo "Setting up cbpolicyd database clean-up daily at 03:35AM in /etc/cron.d/cbpolicyd-cleanup"
 echo "35 3 * * * zimbra bash -l -c '/opt/zimbra/cbpolicyd/bin/cbpadmin --config=/opt/zimbra/conf/cbpolicyd.conf --cleanup' >/dev/null" > /etc/cron.d/cbpolicyd-cleanup
 
 echo "To activate your configuration, run as zimbra user:
-zmprov ms \$(zmhostname) +zimbraServiceEnabled cbpolicyd 
-zmprov ms \$(zmhostname) zimbraMtaEnableSmtpdPolicyd TRUE
+zmprov ms \$(zmhostname) +zimbraServiceEnabled cbpolicyd
 zmprov ms \$(zmhostname) zimbraCBPolicydQuotasEnabled TRUE
 zmcontrol restart
 
 You can find logging here:
-/opt/zimbra/log/cbpolicyd.log"
+tail -f /opt/zimbra/log/cbpolicyd.log"
