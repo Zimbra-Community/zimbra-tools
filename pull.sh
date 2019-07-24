3422a5b9a261b1bb2305af5fc9c190600d94fc31
Barry de Graaff <info@barrydegraaff.tk>
2019-07-24 10:06:30 +0200

    adding repos

--- pull.sh
+++ pull.sh
@@ -75,6 +75,8 @@ git clone ssh://git@github.com/Zimbra-Community/zimlet-provisioning
 git clone ssh://git@github.com/Zimbra-Community/zimbra-foss-2fa
 git clone ssh://git@github.com/Zimbra-Community/reply-by-filter
 git clone ssh://git@github.com/Zimbra-Community/unsplash
+git clone ssh://git@github.com/Zimbra-Community/proxy
+git clone ssh://git@github.com/Zimbra-Community/HPO-Zimlet
 
 cd "$(dirname "$0")"
 ls | grep -v pull.sh | xargs -I{} git -C {} pull
